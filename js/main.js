e6d3ecf6bb936f22e16a076fdd78255c9cc1701e
SeanSassenrath <sean.sassenrath@gmail.com>
2015-08-20 23:54:02 -0700

    adding zipcode weather finder back in

--- js/main.js
+++ js/main.js
@@ -2,45 +2,26 @@ $(document).ready(function() {
   console.log("HELLO!");
   getLocation();
 
-  $('#zip-get-weather').on('click', function(e) {
+  $('#get-zip-weather').on('click', function(e) {
     e.preventDefault();
-    console.log('Testing')
     var zipcode = $('#zipcode').val();
     console.log('Zipcode is ' + zipcode);
+    getZipWeather(zipcode);
   });
 
-    var getWeather = function(zipcode) {
-      $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode +",us,&APPID=d989efa9b6afbb8803d408c2a7168b59",
-        type: 'GET',
-        dataType: 'json',
-      }).done(function(response) {
-        console.log('success', response);
-        var fahrenheit = conversions.fahrenheit(response.main.temp);
-        appendWeather(fahrenheit, response.name);
-      }).fail(function() {
-        console.log('failed ', response);
-      });
-    };
-
-  var appendWeather = function(temp, city) {
-    $('#instructions').hide();
-    $('#temp').empty();
-    $('#city').empty();
-    $('#temp').append(temp + " F");
-    $('#city').append(city);
+  var getZipWeather = function(zipcode) {
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode +",us,&APPID=d989efa9b6afbb8803d408c2a7168b59",
+      type: 'GET',
+      dataType: 'json',
+    }).done(function(response) {
+      console.log('success', response);
+      var fahrenheit = conversions.fahrenheit(response.main.temp);
+      appendWeather(fahrenheit, response.name);
+    }).fail(function() {
+      console.log('failed ', response);
+    });
   };
-
-  var conversions = (function() {
-    var fahrenheit = function(kelvin) {
-      var kToFConversion = ((kelvin - 273.15) * 1.8 + 32);
-      console.log('Converting ' + kelvin + ' Kelvin to ' + kToFConversion + ' Fahrenheit');
-      return (Math.floor((kelvin - 273.15) * 1.8 + 32));
-    }
-    return {
-      fahrenheit: fahrenheit
-    }
-  })()
 
   function getLocation() {
       if (navigator.geolocation) {
@@ -66,4 +47,24 @@ $(document).ready(function() {
       console.log('failed ', response);
     });
   };
+
+  var appendWeather = function(temp, city) {
+    $('#instructions').hide();
+    $('#temp').empty();
+    $('#city').empty();
+    $('#temp').append(temp + " F");
+    $('#city').append(city);
+  };
+
+  var conversions = (function() {
+    var fahrenheit = function(kelvin) {
+      var kToFConversion = ((kelvin - 273.15) * 1.8 + 32);
+      console.log('Converting ' + kelvin + ' Kelvin to ' + kToFConversion + ' Fahrenheit');
+      return (Math.floor((kelvin - 273.15) * 1.8 + 32));
+    }
+    return {
+      fahrenheit: fahrenheit
+    }
+  })()
+
 });
